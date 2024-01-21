51d7f2e21c285b74fc308e2b18fff9660e95de87
victor <victoriess77@gmail.com>
2024-01-20 20:08:03 -0500

    Wire package implementation practice

--- chapter3/go.mod
+++ chapter3/go.mod
@@ -4,6 +4,7 @@ go 1.21.5
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/wire v0.5.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
--- chapter3/main.go
+++ chapter3/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"github.com/victoryRo/test-driven-develop/calculator"
+	"github.com/victoryRo/test-driven-develop/input"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello love")
+	expr := flag.String("expression", "7 + 14", "mathematical expression to parser")
+	flag.Parse()
+
+	engine := calculator.NewEngine()
+	validator := input.NewValidator(engine.GetNumOperands(), engine.GetValidOperators())
+	parser := input.NewParser(engine, validator)
+
+	result, err := parser.ProcessExpression(*expr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(*result)
 }
